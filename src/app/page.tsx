c4fe023e872651754b7fd735c99bb8f35b1a6647
areka06 <116472942+areka06@users.noreply.github.com>
2024-11-19 09:32:08 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useEffect, useRef } from "react"
 import Image from "next/image"
 import Link from "next/link"
